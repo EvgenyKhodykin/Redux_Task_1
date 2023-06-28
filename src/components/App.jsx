8cc59abcfad32f0be459b380843590f13ae8d6b5
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-06-28 17:00:38 +0500

    ui has been updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,35 +24,39 @@ function App() {
 
     return (
         <>
-            <h1 className='mx-3 mb-3'>App</h1>
-
-            <ul>
-                {state.map(element => (
-                    <li key={element.id}>
-                        <p>{element.title}</p>
-                        <p>{`Completed: ${element.completed}`}</p>
-                        <button
-                            className='btn btn-primary'
-                            onClick={() => completeTask(element.id)}
+            <h1 className='mx-3 mt-3'>Redux</h1>
+            <hr />
+            <div className='container-fluid'>
+                <ul className='mt-5'>
+                    {state.map(element => (
+                        <li
+                            key={element.id}
+                            className='mb-5'
                         >
-                            Completed
-                        </button>
-                        <button
-                            className='btn btn-warning mx-2'
-                            onClick={() => changeTitle(element.id)}
-                        >
-                            Change Title
-                        </button>
-                        <button
-                            className='btn btn-danger'
-                            onClick={() => removeTask(element.id)}
-                        >
-                            Delete Title
-                        </button>
-                        <hr />
-                    </li>
-                ))}
-            </ul>
+                            <p>{element.title}</p>
+                            <p>{`Completed: ${element.completed}`}</p>
+                            <button
+                                className='btn btn-primary'
+                                onClick={() => completeTask(element.id)}
+                            >
+                                Completed
+                            </button>
+                            <button
+                                className='btn btn-warning mx-2'
+                                onClick={() => changeTitle(element.id)}
+                            >
+                                Change Title
+                            </button>
+                            <button
+                                className='btn btn-danger'
+                                onClick={() => removeTask(element.id)}
+                            >
+                                Delete Title
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>
     )
 }
