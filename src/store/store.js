887b6dd4b06245f7243db958251d7ddc91aea2ae
EvgenyKhodykin <dr.khodykin@gmail.com>
2023-07-03 21:04:10 +0400

    devTools

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,11 @@ import { legacy_createStore as createStore } from 'redux'
 import tasksReducer from './tasks'
 
 function configureStore() {
-    return createStore(tasksReducer)
+    return createStore(
+        tasksReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 }
 
 export default configureStore
