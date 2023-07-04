70565f6da1fd1438f4392c2d9849077c69b918f7
EvgenyKhodykin <dr.khodykin@gmail.com>
2023-07-04 20:43:01 +0400

    middleware has been added

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,21 @@
-import { legacy_createStore as createStore } from 'redux'
+import {
+    applyMiddleware,
+    legacy_createStore as createStore,
+    compose
+} from 'redux'
 import tasksReducer from './tasks'
+import { logger } from './middleware/logger'
+
+const middlewareEnhancer = applyMiddleware(logger)
 
 function configureStore() {
     return createStore(
         tasksReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
+        compose(
+            middlewareEnhancer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+                window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
     )
 }
 
